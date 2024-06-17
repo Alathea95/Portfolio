341d2c4dc07d25de693a7a95a782ccb808cabf83
JordanVanBuul <drizz1995@hotmail.com>
2024-06-17 13:33:45 +0100

    added a json file that isnt working \o/

---
+++ javascript/javascript.js
@@ -0,0 +1,21 @@
+
+fetch('projects.json').then((res) => {
+    return res.json()
+})
+
+.then((data) => {
+const target = document.querySelector ('.target')
+data.projects.forEach((projects)=> {
+    target.innerHTML += `
+
+    <h4>${projects.Title}</h4>
+
+    <img src="${projects.Img}" />
+
+    <p>${projects.Description}</p> 
+
+    <a href="${projects.Git}">Git</a>
+    `
+})
+
+})
