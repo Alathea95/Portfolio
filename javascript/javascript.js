6f6380dbbe4412834589fa87fa7d8791a54e6561
JordanVanBuul <drizz1995@hotmail.com>
2024-06-18 15:31:34 +0100

    my disappointment is immeasureable, and my day is ruined

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,17 +9,12 @@ data.projects.forEach((projects)=> {
     target.innerHTML += `
 
     <div class="target">
-        <h4>${projects.Title}</h4>    
-    <div>
-            
-            <div class="target2>
-                <p class="portfoliotext">${projects.Description}</p> 
-                <a class="githubpro" href="${projects.Git}"> Github <i class="fa-brands fa-square-github fa-2xl"></i></a>  
-                <img class="portimg" src="${projects.Img}" />
-            </div>
+        <h4>${projects.Title}</h4>           
+        <div class="target2">
+            <p class="portfoliotext">${projects.Description}</p> 
+            <a class="githubpro" href="${projects.Git}"> Github <i class="fa-brands fa-square-github fa-2xl"></i></a>  
+            <img class="portimg" src="${projects.Img}" />
         </div>
-    
-
     </div>
     `
 })
