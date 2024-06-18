2f6a4bf944bc14642fee073abd439c4b71a5e423
JordanVanBuul <drizz1995@hotmail.com>
2024-06-18 14:14:36 +0100

    moments before disaster

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,18 +9,17 @@ data.projects.forEach((projects)=> {
     target.innerHTML += `
 
     <div class="target">
-        <div>
-            <h4>${projects.Title}</h4>
+        <h4>${projects.Title}</h4>    
+    <div>
+            
             <div class="target2>
                 <p class="portfoliotext">${projects.Description}</p> 
-                <div>
-                    <a class="githubpro" href="${projects.Git}"> Github <i class="fa-brands fa-square-github fa-2xl"></i></a>
-                </div>
+                <a class="githubpro" href="${projects.Git}"> Github <i class="fa-brands fa-square-github fa-2xl"></i></a>  
+                <img class="portimg" src="${projects.Img}" />
             </div>
         </div>
     
 
-    <img class="portimg" src="${projects.Img}" />
     </div>
     `
 })
