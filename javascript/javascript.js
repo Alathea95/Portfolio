92ab6ffb6f6ad6dd8da6df4f51ea988d9b773599
JordanVanBuul <drizz1995@hotmail.com>
2024-06-20 10:58:59 +0100

    final build pre more birbs

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,22 +1,19 @@
-
-fetch('javascript/projects.json').then((res) => {
-    return res.json()
-})
-
+fetch('javascript/projects.json')
+    .then((res) => {
+        return res.json();
+    })
     .then((data) => {
-        const target = document.querySelector('.portfolios')
-        data.projects.forEach((projects) => {
+        const target = document.querySelector('.portfolios');
+        data.projects.forEach((project) => {
             target.innerHTML += `
-
-    <div class="target">
-        <h4>${projects.Title}</h4>           
-        <div class="target2">
-            <p class="portfoliotext">${projects.Description}</p> 
-            <a target="_blank" class="githubpro" href="${projects.Git}"> Github <i class="fa-brands fa-square-github fa-2xl"></i></a>  
-            <img class="portimg" src="${projects.Img}" />
-        </div>
-    </div>
-    `
+                <div class="target">
+                    <h4>${project.Title}</h4>           
+                    <div class="target2">
+                        <p class="portfoliotext">${project.Description}</p> 
+                        <a target="_blank" class="githubpro" href="${project.Git}"> Github <i class="fa-brands fa-square-github fa-2xl"></i></a>  
+                        <img class="portimg" src="${project.Img}" alt="${project.Title}" />
+                    </div>
+                </div>
+            `
         })
-
     })
